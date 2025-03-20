e670afa29309f6f16ead700ab1f9896d0c6d7302
Joskul <hikaru.kp@gmail.com>
2025-03-20 12:54:27 +0700

    Restructure WIP

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1>Hello World!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
